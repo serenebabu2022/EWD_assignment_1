aaa4a439f3cff64e03bc0eedb6b9366253e80c33
serenebabu2022 <serenebabu22@gmail.com>
2024-03-07 16:55:41 +0000

    Initial commit

---
+++ test/serverless_basic_assignment.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessBasicAssignment from '../lib/serverless_basic_assignment-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless_basic_assignment-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessBasicAssignment.ServerlessBasicAssignmentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
