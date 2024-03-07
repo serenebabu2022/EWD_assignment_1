5fb36632c67a20b7a490974e59f774d498a89cd3
serenebabu2022 <serenebabu22@gmail.com>
2024-03-07 17:41:39 +0000

    Starting with lambdas

--- lib/serverless_basic_assignment-stack.ts
+++ lib/serverless_basic_assignment-stack.ts
@@ -1,16 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ServerlessBasicAssignmentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const simpleFn = new lambdanode.NodejsFunction(this, "SimpleFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/simple.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+    });
+    const simpleFnURL = simpleFn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.AWS_IAM,
+      cors: {
+        allowedOrigins: ["*"],
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessBasicAssignmentQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, "Simple Function Url", { value: simpleFnURL.url });
   }
 }
